f41c4fc99ac75cceb3d36dda69d9193956cf2ded
flag <deksyemretsson@gmail.com>
2026-09-14 00:05:12 +0200

    Create helplabel.lua for command bar UI

---
+++ helplabel.lua
@@ -0,0 +1,18 @@
+local cmd32_ScreenGui = Instance.new("ScreenGui")
+cmd32_ScreenGui.Name = "cmd32"
+cmd32_ScreenGui.Parent = game.Players.LocalPlayer.PlayerGui
+	
+local cmdbar_TextBox = Instance.new("TextBox")
+cmdbar_TextBox.Name = "cmdbar"
+cmdbar_TextBox.Size = UDim2.new(0, 1914, 0, 15)
+cmdbar_TextBox.Position = UDim2.new(-0.000431327847763896, 0, 0.9810844659805298, 0)
+cmdbar_TextBox.BackgroundColor3 = Color3.new(0, 0, 0)
+cmdbar_TextBox.Text = ""
+cmdbar_TextBox.TextColor3 = Color3.new(1, 1, 1)
+cmdbar_TextBox.TextSize = 14
+cmdbar_TextBox.AnchorPoint = Vector2.new(0, 0)
+cmdbar_TextBox.Visible = true
+cmdbar_TextBox.BackgroundTransparency = 0
+cmdbar_TextBox.BorderSizePixel = 0
+cmdbar_TextBox.BorderColor3 = Color3.new(0, 0, 0)
+cmdbar_TextBox.Parent = cmd32_ScreenGui
