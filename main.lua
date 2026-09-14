c53ed5945612dc3290d26443e2a5e4b59343e848
flag <deksyemretsson@gmail.com>
2026-09-14 23:15:19 +0200

    Add new commands and update existing ones

--- main.lua
+++ main.lua
@@ -21,10 +21,10 @@ cmdbar_TextBox.Parent = cmd32_ScreenGui
 
 local Commands = {
 	["help"] = "https://raw.githubusercontent.com/flag7qc/cmd32/refs/heads/main/help.lua",
+	["hubs"] = "",
+	["commands"] = "",
     ["fly"] = "https://raw.githubusercontent.com/flag7qc/cmd32/refs/heads/main/fly.lua",
-    ["unfly"] = "https://raw.githubusercontent.com/flag7qc/cmd32/refs/heads/main/fly.lua",
-	["noclip"] = "https://raw.githubusercontent.com/flag7qc/cmd32/refs/heads/main/noclip.lua",
-	["unnoclip"] = "https://raw.githubusercontent.com/flag7qc/cmd32/refs/heads/main/noclip.lua"
+    ["unfly"] = "https://raw.githubusercontent.com/flag7qc/cmd32/refs/heads/main/fly.lua"
 }
 
 cmdbar_TextBox.FocusLost:Connect(function(enterPressed)
