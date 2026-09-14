339b41444ff362285432024fb1133dbf8d1d3c29
flag <deksyemretsson@gmail.com>
2026-09-14 22:26:05 +0200

    Fix loader.lua to load main.lua correctly

--- loader.lua
+++ loader.lua
@@ -1,2 +1,1 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/flag7qc/cmd32/refs/heads/main/main.lua"))()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/flag7qc/cmd32/refs/heads/main/info.lua"))()
