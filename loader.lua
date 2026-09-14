d9956a21dd378bcffd38382d8efbb56e957587b1
flag <deksyemretsson@gmail.com>
2026-09-14 22:16:57 +0200

    Load external scripts from GitHub in loader.lua

--- loader.lua
+++ loader.lua
@@ -1,1 +1,2 @@
-
+loadstring(game:HttpGet("https://raw.githubusercontent.com/flag7qc/cmd32/refs/heads/main/main.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/flag7qc/cmd32/refs/heads/main/info.lua"))()
